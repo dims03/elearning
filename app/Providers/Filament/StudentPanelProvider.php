c11750dc81d76f6e8e5ad970f15e3b106e8e9bef
DenDims <dimasp95@icloud.com>
2026-05-30 09:46:34 +0800

    feat: update login student ui

--- app/Providers/Filament/StudentPanelProvider.php
+++ app/Providers/Filament/StudentPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Student\Pages\Auth\Login;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,7 +27,8 @@ class StudentPanelProvider extends PanelProvider
         return $panel
             ->id('student')
             ->path('student')
-            ->login()
+            ->login(Login::class)
+            ->spa(hasPrefetching: true)
             ->colors([
                 'primary' => Color::Amber,
             ])
